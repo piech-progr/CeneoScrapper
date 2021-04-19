e1ff3536f585900efc91e5c2a8821cb8e44f8ee6
Maciej <piech.progr@gmail.com>
2021-04-19 13:57:52 +0200

    2021-04-12 MP

--- main.py
+++ main.py
@@ -1,44 +1,85 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-respons = requests.get("https://www.ceneo.pl/70068802#tab=reviews")
+extracted_opinions= []
+
+product_id=input("Podaj kod produktu: ")
+next_page="https://www.ceneo.pl/{}#tab=reviews".format(product_id)
+
+while next_page:
+
+    respons = requests.get(next_page)
 
 
-page_dom = BeautifulSoup(respons.text, "html.parser")
+    page_dom = BeautifulSoup(respons.text, "html.parser")
 
-opinion = page_dom.select("div.js_product-review").pop(0)
 
-opinion_id = opinion["data-entry-id"]
-author = opinion.select("span.user-post__author-name").pop(0).get_text().strip()
-try:
-    recomedation = opinion.select("span.user-post__author-recomendation > em").pop(0).get_text().strip()
-    recomedation = True if recomedation=="Polecam" else False
-except IndexError:
-    recomendation = None
-stars = opinion.select("span.user-post__score-count").pop(0).get_text().strip()
-stars = float(stars.split("/")[0].replace(",","."))
-content = opinion.select("div.user-post__text").pop(0).get_text().strip()
-try:
-    pros = opinion.select("div.review-feature_col:has(>div[class*=\"possitives\"])>div.review-feature_item")
-    pros = [x.get_text().strip() for x in pros]
-except IndexError:
-    pros = None
-try:
-    cons = opinion.select("div.review-feature_col:has(>div[class*=\"negatives\"])>div.review-feature_item")
-    [x.get_text().strip() for x in cons]
-except IndexError:
-    cons = None
-try:
-    purchased = bool(opinion.select("div.review-pz").pop(0).get_text().strip())
-except IndexError:
-    purchased = None
-submit_date = opinion.select("span.user-post__published > time:nth-child(1)").pop(0)["datetime"].strip()
-try:
-    purchase_date = opinion.select("span.user-post__published > time:nth-child(2)").pop(0)["datetime"].strip()
-except IndexError:
-    purchase_date = None
-useful = int(opinion.select("span[id^='votes-yes']").pop(0).get_text().strip())
-useless = int(opinion.select("span[id^='votes-no']").pop(0).get_text().strip())
+    # opinion=opinions.pop(0)
 
-print( author, recomedation, stars, content, pros, cons, purchased, submit_date, purchase_date, useful, useless, sep="\n")
-print(type(opinion))
+    opinions = page_dom.select("div.js_product-review").pop(0)
+
+    for opinion in opinions:
+
+        opinion_id = opinion["data-entry-id"]
+        author = opinion.select("span.user-post__author-name").pop(0).get_text().strip()
+        try:
+            recomedation = opinion.select("span.user-post__author-recomendation > em").pop(0).get_text().strip()
+            recomedation = True if recomedation=="Polecam" else False
+        except IndexError:
+            recomendation = None
+        stars = opinion.select("span.user-post__score-count").pop(0).get_text().strip()
+        stars = float(stars.split("/")[0].replace(",","."))
+        content = opinion.select("div.user-post__text").pop(0).get_text().strip()
+        try:
+            pros = opinion.select("div.review-feature_col:has(>div[class*=\"possitives\"])>div.review-feature_item")
+            pros = [x.get_text().strip() for x in pros]
+        except IndexError:
+            pros = None
+        try:
+            cons = opinion.select("div.review-feature_col:has(>div[class*=\"negatives\"])>div.review-feature_item")
+            [x.get_text().strip() for x in cons]
+        except IndexError:
+            cons = None
+        try:
+            purchased = bool(opinion.select("div.review-pz").pop(0).get_text().strip())
+        except IndexError:
+            purchased = None
+        submit_date = opinion.select("span.user-post__published > time:nth-child(1)").pop(0)["datetime"].strip()
+        try:
+            purchase_date = opinion.select("span.user-post__published > time:nth-child(2)").pop(0)["datetime"].strip()
+        except IndexError:
+            purchase_date = None
+        useful = int(opinion.select("span[id^='votes-yes']").pop(0).get_text().strip())
+        useless = int(opinion.select("span[id^='votes-no']").pop(0).get_text().strip())
+
+
+        opinion_elements = {
+                    "opinion_id": opinion_id,
+                    "author": author,
+                    "recommendation": recommendation,
+                    "stars": stars,
+                    "content": content,
+                    "pros": pros,
+                    "cons": cons,
+                    "purchased": purchased,
+                    "submit_date": submit_date,
+                    "purchase_date": purchase_date,
+                    "useful": useful,
+                    "useless": useless
+                }
+
+        extracted_opinions.append(opinion_elements)
+    try:
+        next_page= "https://www.ceneo.pl" + \
+        page_dom.select("a.pagination_next").pop()["href"]
+    except IndexError:
+        next_page=None
+
+        # print(extracted_opinions)
+    print(next_page)
+
+with open(f"./opinions/{product_id}.json","w",encoding="UTF-8") as fp:
+    json.dumps(extracted_opinions, fp,indent=4, ensure_ascii=False)
+
+ 
